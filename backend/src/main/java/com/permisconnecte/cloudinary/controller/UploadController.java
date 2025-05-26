b85931c1dd70d05e725ed346c611bc8ae801e9dd
Badrbernane <151386759+Badrbernane@users.noreply.github.com>
2025-05-26 02:37:27 +0100

    add clouddata

---
+++ backend/src/main/java/com/permisconnecte/cloudinary/controller/UploadController.java
@@ -0,0 +1,26 @@
+package com.permisconnecte.cloudinary.controller;
+
+import com.permisconnecte.cloudinary.service.CloudinaryService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/upload")
+public class UploadController {
+
+    private final CloudinaryService cloudinaryService;
+
+    public UploadController(CloudinaryService cloudinaryService) {
+        this.cloudinaryService = cloudinaryService;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+        Map uploadResult = cloudinaryService.uploadFile(file);
+        return ResponseEntity.ok(uploadResult);
+    }
+} 
